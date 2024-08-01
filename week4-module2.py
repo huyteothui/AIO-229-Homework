3c55f77d194e5a09de2672bbfc7efa904cc61d74
huyteothui <119801969+huyteothui@users.noreply.github.com>
2024-08-02 00:33:08 +0700

    Update week4-module2.py

--- week4-module2.py
+++ week4-module2.py
@@ -1,5 +1,7 @@
 import numpy as np
 from math import sqrt
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 ### Question 1
 def compute_mean(arr: np.array):
@@ -23,3 +25,51 @@ def compute_std(arr: np.array):
     for x in arr:
         values.append((x - mean) ** 2)
     return sqrt(sum(values) / n)
+
+### Question 4
+def compute_correlation_coefficient(x, y):
+    n = len(x)
+    numerator = 0
+    denominator = 0
+
+    std_x = compute_std(x) 
+    std_y = compute_std(y)
+
+    mean_x = compute_mean(x)
+    mean_y = compute_mean(y)
+
+    temp_total = 0
+    for i in range(n):
+        temp_total += ((x[i] - mean_x) * (y[i] - mean_y))
+    
+    numerator = temp_total / n
+
+    denominator += std_x * std_y
+    
+    return numerator / denominator
+
+# Cau 10
+def tfidf_search(question, tfidf_vectorizer, top_d=5):
+    query_embedded = tfidf_vectorizer.transform([question.lower()])
+    cosine_scores = cosine_similarity(context_embedded, query_embedded).reshape((-1,))
+    results = []
+    for idx in cosine_scores.argsort()[-top_d:][::-1]:
+        doc = {
+            'id': idx,
+            'cosine_score':cosine_scores[idx]
+        }
+        results.append(doc)
+    return results
+
+#Cau 12
+def tfidf_search(question, tfidf_vectorizer, top_d=5):
+    query_embedded = tfidf_vectorizer.transform([question.lower()])
+    cosine_scores = cosine_similarity(context_embedded, query_embedded).reshape((-1,))
+    results = []
+    for idx in cosine_scores.argsort()[-top_d:][::-1]:
+        doc = {
+            'id': idx,
+            'cosine_score':cosine_scores[idx]
+        }
+        results.append(doc)
+    return results
